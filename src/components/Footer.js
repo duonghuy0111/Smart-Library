f626d9035e88efde8c6c9dfe978c8e7178718be0
TminTtapcode <oakceniki@gmail.com>
2026-05-24 17:16:56 +0700

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,4 +0,0 @@
-const Footer = () => {
-    return <div className="p-4 mt-5 bg-dark text-white text-center">© 2026 Thư Viện Số</div>
-}
-export default Footer;
--- src/configs/Contexts.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export const MyUserContext = createContext();
-export const MyCartContext = createContext(); // Thêm dòng này vào
